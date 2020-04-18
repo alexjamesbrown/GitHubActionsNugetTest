a326cf457c220e37966f4831f2db5a8369c428a3
Alex Brown <alex@cohoda.com>
2020-04-18 23:07:27 +0100

    add tests

---
+++ test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Nunit.Framework;
+
+namespace GitHubActionsNugetTest.UnitTest
+{
+    public class Tests
+    {
+        public void Test1()
+        {
+            //wow great test
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
